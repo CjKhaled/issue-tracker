32d5f717abc57465a7c40f5c578d6a22aac02a08
Cj Alexander <clauvinsalexander@gmail.com>
2024-09-10 17:14:15 -0400

    initial setup

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
 // Entry point
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('This is the beginning.'))
+
+app.listen(3000, () => console.log('server listening on port 3000!'))
